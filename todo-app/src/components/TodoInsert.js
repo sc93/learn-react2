fae3a1b9ec516e3c810cece8686a1e654f168e27
SC <topsky2008@gmail.com>
2020-07-04 17:21:28 +0900

    todo app

---
+++ todo-app/src/components/TodoInsert.js
@@ -0,0 +1,31 @@
+import React, { useState, useCallback } from "react";
+import { MdAdd } from "react-icons/md";
+import "./TodoInsert.scss";
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState("");
+    const onChange = (e) => {
+        setValue(e.target.value);
+    };
+    const onSubmit = useCallback(
+        (e) => {
+            e.preventDefault();
+            onInsert(value);
+            setValue("");
+        },
+        [onInsert, value],
+    );
+    return (
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input
+                placeholder="할 일을 입력해주세요."
+                value={value}
+                onChange={onChange}
+            />
+            <button type="submit">
+                <MdAdd />
+            </button>
+        </form>
+    );
+};
+
+export default TodoInsert;
