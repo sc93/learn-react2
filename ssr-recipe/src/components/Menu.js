8c1936ad0cbcda0ef94e8122249550fd03d051f9
SC <topsky2008@gmail.com>
2020-08-05 22:29:11 +0900

    Commit before eject ssr-tutorial

---
+++ ssr-recipe/src/components/Menu.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Menu = () => {
+    return (
+        <ul>
+            <li>
+                <Link to="./Red">Red</Link>
+            </li>
+            <li>
+                <Link to="./Blue">Blue</Link>
+            </li>
+        </ul>
+    );
+};
+
+export default Menu;
